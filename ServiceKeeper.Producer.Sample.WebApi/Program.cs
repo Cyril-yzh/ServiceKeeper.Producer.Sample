c3f29c5e361b99ae3f444c5f18674d05aac90109
Cyril <Cyril-yzh@yandex.com>
2023-10-18 16:17:02 +0800

    Version-1.0.0

--- ServiceKeeper.Producer.Sample.WebApi/Program.cs
+++ ServiceKeeper.Producer.Sample.WebApi/Program.cs
@@ -23,9 +23,9 @@ namespace ServiceKeeper.Producer.Sample.WebApi
             builder.Services.Configure<ServiceKeeperOptions>(options =>
             {
                 options.MQHostName = "vyzh2019";
-                options.MQExchangeName = "echangeEventBusDemo1";
+                options.MQExchangeName = "ServiceKeeper";
                 options.MQUserName = "admin";
-                options.MQPassword = "Aa111111";
+                options.MQPassword = "password";
                 options.ServiceDescription = "服务调度中心";
             });
             builder.Services.Configure<ServiceKeeperUIOptions>(options =>
@@ -33,11 +33,10 @@ namespace ServiceKeeper.Producer.Sample.WebApi
                 options.IsTakeOverTaskScheduling = true;
                 options.CreateJwtSecretKey("CreateJwtSecretKey");
                 options.AllowedCredentials.TryAdd("username", "password");
-                options.AllowedCredentials.TryAdd("yuzehao", "Aa111111");
                 options.ServiceKeeperUrl = url;
             });
 
-            builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("192.168.23.4:6379,password=Sivic2812"));
+            builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("192.168.23.4:6379,password=password"));
 
             //serviceKeeper注入
             builder.Services.AddProducerServiceKeeper(null);
@@ -48,7 +47,6 @@ namespace ServiceKeeper.Producer.Sample.WebApi
                 options.AddDefaultPolicy(builder =>
                 {
                     builder.WithOrigins().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-                    //builder.WithOrigins(/*url,*/"https://192.168.23.4:17777/", "http://127.0.0.1:5500", "http://localhost:5500", "http://192.168.23.4:5500", "http://127.0.0.1:5501", "http://localhost:5501", "http://192.168.23.4:5501", "null").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
                 });
             });
 
@@ -63,7 +61,6 @@ namespace ServiceKeeper.Producer.Sample.WebApi
             app.UseProducerServiceKeeper();
             app.UseServiceKeeperUI();
 
-            //app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
